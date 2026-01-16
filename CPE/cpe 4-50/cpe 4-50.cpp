8b1c98b3096674dc8592b5f6b678b975c2fe92fd
yuanho-0211 <ho950211@gmail.com>
2026-01-16 16:29:14 +0800

    Update cpe 4-50.cpp

--- CPE/cpe 4-50/cpe 4-50.cpp
+++ CPE/cpe 4-50/cpe 4-50.cpp
@@ -1,3 +1,4 @@
+error
 #include<iostream>
 using namespace std;
 int main()
@@ -29,3 +30,63 @@ int main()
 	cout << a << " "<<b << " "<<min << endl;
 	return 0;
 }
+--------------------------------------------------
+	#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	int min = 0;
+	for (int i = a; i <= b; i++)
+	{
+		int c = 0;
+		int n = i;
+		while (n != 1)
+		{
+			if (n % 2 == 1)
+			{
+				n = 3 * n + 1;
+				c++;
+			}
+			else
+			{
+				n = n / 2;
+				c++;
+			}
+	    }
+		c = c + 1;
+		if(c>min)
+		{
+			min = c;
+		}
+	}
+	cout << a << " "<<b << " "<<min << endl;
+	return 0;
+}
+---------------------------------------------------
+	#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    while (cin >> a >> b) {
+        int maxCycle = 0; // 儲存最大循環長度
+        for (int i = a; i <= b; i++) {
+            int n = i;
+            int c = 1; // 從自己開始算
+            while (n != 1) {
+                if (n % 2 == 1)
+                    n = 3 * n + 1;
+                else
+                    n = n / 2;
+                c++;
+            }
+            if (c > maxCycle)
+                maxCycle = c;
+        }
+        cout << a << " " << b << " " << maxCycle << endl;
+    }
+    return 0;
+}
+

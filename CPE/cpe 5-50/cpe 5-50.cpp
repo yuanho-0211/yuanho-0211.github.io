ec3476e564326dab7c64420c7b2f68fc606bac7f
yuanho-0211 <ho950211@gmail.com>
2026-01-16 17:08:43 +0800

    Update cpe 5-50.cpp

--- CPE/cpe 5-50/cpe 5-50.cpp
+++ CPE/cpe 5-50/cpe 5-50.cpp
@@ -20,3 +20,29 @@ int main()
 		
 	}
 }
+----------------------------------------------------------
+	#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    while (cin >> s) {
+        if (s == "0") break; // 輸入結束
+
+        int oddSum = 0, evenSum = 0;
+        for (int i = 0; i < s.size(); i++) {
+            int digit = s[i] - '0';
+            if (i % 2 == 0)
+                oddSum += digit;   // 字串第一位算作位置 0（奇數位）
+            else
+                evenSum += digit;
+        }
+
+        if ((oddSum - evenSum) % 11 == 0 || (oddSum - evenSum) % 11 == -0)
+            cout << s << " is a multiple of 11." << endl;
+        else
+            cout << s << " is not a multiple of 11." << endl;
+    }
+    return 0;
+}

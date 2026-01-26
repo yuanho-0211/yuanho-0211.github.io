9deb8b35d1cd07440bc595d6df0cb427d7316582
yuanho-0211 <ho950211@gmail.com>
2026-01-26 15:46:41 +0800

    Update cpe 15-50.cpp

--- CPE/cpe 15-50/cpe 15-50.cpp
+++ CPE/cpe 15-50/cpe 15-50.cpp
@@ -1,42 +1,45 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cmath>
 using namespace std;
 int main()
 {
-	
+
 	string s;
-	string o;
+	vector <int> v;
 	int p;
-	while (getline(cin,s))
+	while (getline(cin, s))
 	{
-		int k = 1;
-	     p = 0;
-		 bool t = false;
+		
+		p = 0;
+		bool t = false;
 		for (int i = 0; i < s.size(); i++)
 		{
 			if (s[i] != ' ')
 			{
-				o[p] = s[i];
+				v.push_back(s[i]);
 				p++;
 			}
 		}
-		while (k < o.size())
+		int k = 1;
+		for (int i = 0; i < p; i++)
 		{
-			for (int i = 0; i <p; i++)
+			while(abs(v[i]-v[i+1])==k)
 			{
-				if (o[i] == 'k' )
-				{
 					t = true;
 					k++;
-					continue;
-				}
 			}
-			if (!t)
+			if (t)
 			{
-				break;
+				continue;
+			}
+			else
+			{
+				t = false;
 			}
 		}
-		
+
 		if (t)
 		{
 			cout << "Jolly";
@@ -46,4 +49,69 @@ int main()
 			cout << "Not jolly";
 		}
 	}
+}
+---------------------------------------------
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    string s;
+    vector<int> v;
+    int p;
+
+    while (getline(cin, s))
+    {
+        v.clear();   // 讀新行前清空
+        p = 0;
+        bool t = true; // 先假設是 Jolly
+
+        // 用 stringstream 讀整數
+        stringstream ss(s);
+        ss >> p;
+
+        for (int i = 0; i < p; i++)
+        {
+            int num;
+            ss >> num;
+            v.push_back(num);
+        }
+
+        // n=1 一定是 Jolly
+        if (p == 1)
+        {
+            cout << "Jolly\n";
+            continue;
+        }
+
+        // 對每個 k 進行檢查
+        for (int k = 1; k < p; k++)
+        {
+            bool found = false;
+
+            for (int i = 0; i < p - 1; i++)
+            {
+                if (abs(v[i] - v[i + 1]) == k)
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                t = false;
+                break;
+            }
+        }
+
+        if (t)
+            cout << "Jolly\n";
+        else
+            cout << "Not jolly\n";
+    }
 }

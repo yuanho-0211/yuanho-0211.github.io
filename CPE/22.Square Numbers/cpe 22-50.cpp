c0bd7cde581ff4f50e63be2d01b271287b90c5ca
yuanho-0211 <ho950211@gmail.com>
2026-01-31 00:34:52 +0800

    Update cpe 22-50.cpp

--- CPE/22.Square Numbers/cpe 22-50.cpp
+++ CPE/22.Square Numbers/cpe 22-50.cpp
@@ -25,3 +25,23 @@ int main()
 		}
 	}
 }
+-------------------------------------------------
+	#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int a, b;
+    while (cin >> a >> b) {
+        if (a == 0 && b == 0) break;
+
+        int L = ceil(sqrt(a));
+        int R = floor(sqrt(b));
+
+        if (L > R)
+            cout << 0 << endl;
+        else
+            cout << (R - L + 1) << endl;
+    }
+    return 0;
+}

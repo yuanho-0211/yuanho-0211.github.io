b2869426b82c7e155f8feb39d7fb82d2d4fb06e1
yuanho-0211 <ho950211@gmail.com>
2026-01-31 23:25:58 +0800

    Update cpe 23-50.cpp

--- CPE/23.B2-Sequence/cpe 23-50.cpp
+++ CPE/23.B2-Sequence/cpe 23-50.cpp
@@ -61,3 +61,77 @@ int main()
 	
 }
 -----------------------------------------------------------------------------
+	#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    int n, a;
+    int m = 1;
+
+    while (cin >> n)
+    {
+        vector<int> v;
+        set<int> s;
+        bool t = false;
+
+        // 讀入資料
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a;
+            v.push_back(a);
+        }
+
+        if (v[0] < 1)
+        {
+            t = true;
+           
+        }
+        // 檢查是否嚴格遞增
+        for (int i = 0; i < v.size() - 1; i++)
+        {
+            
+            if (v[i] >= v[i + 1])
+            {
+                t = true;
+                break;
+            }
+        }
+
+        // 計算所有 pairwise sums
+        if (!t)
+        {
+            for (int i = 0; i < v.size(); i++)
+            {
+                for (int j = i; j < v.size(); j++)
+                {
+                    int sum = v[i] + v[j];
+
+                    // set 裡已經有 → 重複
+                    if (s.count(sum))
+                    {
+                        t = true;
+                        break;
+                    }
+                    s.insert(sum);
+                }
+                if (t) break;
+            }
+        }
+
+        // 輸出
+        cout << "Case #" << m << ": ";
+        if (t)
+            cout << "It is not a B2-Sequence." << endl << endl;
+        else
+            cout << "It is a B2-Sequence." << endl << endl;
+
+        m++;
+    }
+
+    return 0;
+}
+
